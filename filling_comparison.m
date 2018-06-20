aec1b8454dd8393df323f7c4b9a3d5ba8ccbb603
Omer <hazon@stanford.edu>
2018-06-19 21:33:22 -0700

    final touches

--- filling_comparison.m
+++ filling_comparison.m
@@ -75,28 +75,46 @@ figure;
 ylab = 'RMS error (cm)';
 bsep({'Place Decoders'' Performance on Box (nonlocal split)', 'Place Decoders'' Performance on Traces (nonlocal split)'}, meas_test, meas_train, Inf, {{algs{1}.short},{algs{2}.short}}, ylab);
 %% FILLING_COMPARISON V3 : using bin center interpolation -- DEBUGGING
-PL = 4;
-
-%ds = quick_ds('../open_field/om3-0327', 'nocells');
-ds = quick_ds('../open_field/c11m1d25', 'nocells');
+PL = 16;
+PAR = true;
+%ds = quick_ds('../open_field/om2-0405', 'nocells');
+%ds = quick_ds('../open_field/c11m1d25', 'nocells');
 fillings = {'traces'};
-algs{1} = [my_algs('lda'), my_algs('dlda'), my_algs('dqda')];
+algs = [my_algs('lda'), my_algs('dlda'), my_algs('dqda')];
 for ix = 1:numel(fillings)
-    [X, ~, ~] = ds_dataset(ds, 'filling', fillings{ix},...
-        'openfield', true, 'sparsify', ~strcmp(fillings{ix}, 'traces'));
-    y = ds.trials.centroids;
+    %[X, ~, ~] = ds_dataset(ds, 'filling', fillings{ix},...
+    %    'openfield', true, 'sparsify', ~strcmp(fillings{ix}, 'traces'));
+    %y = ds.trials.centroids;
+    y = E_T{6}.tracesEvents.position; %(T, 2)
+    if max(max(y)) > 1e3
+        continue;
+    end
+    X = E_T{6}.tracesEvents.rawTraces; %(T, C)
     %[ks, centers, dims, scXY, ~] = gen_place_bins(ds, 8, 46);
     %errf_xy = @(xy,p) sqrt(mean(sum((xy - centers(p(:),:)).^2,2)));
-    for i = 1:numel(algs{ix})
+    for i = 1:numel(algs)
     %    [train_err{ix,i}, test_err{ix,i}] = evaluate_alg(algs{ix}(i),...
     %        X, ks, 'eval_f_xy', errf_xy, 'XY', scXY, 'train_frac', 0.7, 'par_loops', PL);
-        [meas_train{ix,i}, meas_test{ix,i}, models{ix,i}] = evala(algs{ix}(i), X, y, @(y) gen_place_bins(y,8,46),...
-            'repeats', PL, 'verbose', true, 'interp', false, 'split', 'nonlocal');
+        [meas_train{ix,i}, meas_test{ix,i}, ~] = evala(algs(i), X, y, @(y) gen_place_bins(y,20,120),...
+            'repeats', PL, 'verbose', true, 'interp', false, 'split', 'nonlocal', 'use_par', PAR);
+        [meas_train_interp{ix,i}, meas_test_interp{ix,i}, ~] = evala(algs(i), X, y, @(y) gen_place_bins(y,20,120),...
+            'repeats', PL, 'verbose', true, 'interp', true, 'split', 'nonlocal', 'use_par', PAR);
     end
     fprintf('Completed filling: %s\n', fillings{ix});
 end
 
+% 
+% figure;
+% ylab = 'RMS error (cm)';
+% bsep({'Place Decoders'' Performance on Traces'}, meas_test, meas_train, Inf, {{algs.short}}, ylab);
+% 
+% 
+% figure;
+% ylab = 'RMS error (cm)';
+% bsep({'Place Decoders'' Performance on Traces, with bin interpolation'}, meas_test_interp, meas_train_interp, Inf, {{algs.short}}, ylab);
+
 
 figure;
 ylab = 'RMS error (cm)';
-bsep({'Place Decoders'' Performance on Traces'}, meas_test, meas_train, Inf, {{algs{1}.short}}, ylab);
+bsep({'Place Decoders'' Performance on Traces', 'Place Decoders'' Performance on Traces, with bin interpolation'},...
+    [meas_test;meas_test_interp], [meas_train;meas_train_interp], 8, {{algs.short},{algs.short}}, ylab);
