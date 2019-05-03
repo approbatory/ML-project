d50bf02f75800acbea5c537a770abd5e9cf5855a
Omer Hazon <omer.hazon@berkeley.edu>
2019-05-02 17:39:48 -0700

    remapped parameters in parallel unit

--- sherlock_parallel_unit.m
+++ sherlock_parallel_unit.m
@@ -1,7 +1,7 @@
-function sherlock_parallel_unit(random_str, source, numer, denom)
+function sherlock_parallel_unit(random_str, source, is_padded, distance_cutoff)
 my_rand_seed = str2double(random_str);
-numer = str2double(numer);
-denom = str2double(denom);
+is_padded = str2double(is_padded);
+distance_cutoff = str2double(distance_cutoff);
 rng(my_rand_seed);
 fprintf('random seed %d\n\n', my_rand_seed);
 addpath utils decoding
@@ -10,7 +10,7 @@ try
     %Analyzer.dispatch_update(source, numer, denom);
     %test_selector(str2num(source));
     %DecodeTensor.dispatch_datasize(str2double(source));
-    DecodeTensor.dispatch(str2double(source), true);
+    DecodeTensor.dispatch(str2double(source), is_padded, distance_cutoff);
 catch me
     fprintf('%s / %s\n', me.identifier, me.message);
 end
