b38972fddb48d36cbd9dc90c05ed28577b553855
Omer Hazon <hazon@stanford.edu>
2019-08-05 12:47:32 -0700

    still division bug in mlspike usage

--- spike_detect.m
+++ spike_detect.m
@@ -1,7 +1,7 @@
 function res = spike_detect(index)
 dt = 1/20;
-path_info = DecodeTensor.cons_filt(70);
+path_info = DecodeTensor.cons_filt(70, true);
 load(path_info{1});
 res.spikes = mlspike_yescal(tracesEvents.rawTraces(:, index), dt);
-
+res.cell_index = index;
 end
